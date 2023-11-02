a5d60f2a21ff67fe15806e2b9a24706271ecff19
Mustafin_A <pupenkinvas@yandex.ru>
2023-11-02 15:27:22 +0300

    First attempt in OOP transfer

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
 
+class Character:
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self):
+        ...
+
+    def defence(self):
+        ...
+
+    def special(self):
+        ...
+
+
+
+class Warrior(Character):
+    ...
+
+
+class Mage(Character):
+    ...
+
+
+class Healer(Character):
+    ...
+
+
 def attack(char_name: str, char_class: str) -> str:
     """Calculate damage."""
     if char_class == 'warrior':
